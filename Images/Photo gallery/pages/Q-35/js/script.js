9b58f91f895fb1939d33bcb0b3763b06e14b0fde
Vishwas <54718558+VishWas-13@users.noreply.github.com>
2021-06-13 23:29:44 +0530

    Add files via upload

---
+++ Images/Photo gallery/pages/Q-35/js/script.js
@@ -0,0 +1,203 @@
+function E()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.E </span>: <br> The mathematical constant e. This is Eulers number, the base of natural logarithms.<br> <br> <span class='example'>Example: </span>"+ Math.E;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/E'>Learn More</a>";
+}
+function LN10()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.LN10 </span>: <br> The natural logarithm of 10.<br> <br> <span class='example'>Example: </span>"+ Math.LN10;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/LN10'>Learn More</a>";
+}
+function LN2()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.LN2 </span>: <br> The natural logarithm of 2.<br> <br> <span class='example'>Example: </span>"+ Math.LN2;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/LN2'>Learn More</a>";
+}
+function LOG10E()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.LOG10E </span>: <br> The base-10 logarithm of e.<br> <br> <span class='example'>Example: </span>"+ Math.LOG10E;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/LOG10E'>Learn More</a>";
+}
+function LOG2E()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.LOG2E </span>: <br> The base-2 logarithm of e.<br> <br> <span class='example'>Example: </span>"+ Math.LOG2E;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/LOG2E'>Learn More</a>";
+}
+function PI()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.PI </span>: <br> Pi. This is the ratio of the circumference of a circle to its diameter.<br> <br> <span class='example'>Example: </span>"+ Math.PI;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI'>Learn More</a>";
+}
+function SQRT1_2()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.SQRT1_2 </span>: <br> The square root of 0.5, or, equivalently, one divided by the square root of 2.<br> <br> <span class='example'>Example: </span>"+ Math.SQRT1_2;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/SQRT1_2'>Learn More</a>";
+}
+function SQRT2()
+{
+    explain.innerHTML = "<span class = 'property'>(property)</span> <br> <br> <span class = 'tile'> Math.SQRT2 </span>: <br> The mathematical constant e. This is Eulers number, the base of natural logarithms.<br> <br> <span class='example'>Example: </span>"+ Math.SQRT2;
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/SQRT2'>Learn More</a>";
+}
+function abs()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.abs </span>: <br> Returns the absolute value of a number (the value without regard to whether it is positive or negative). For example, the absolute value of -5 is the same as the absolute value of 5.<br> <br> <span class='example'>Example: abs of -786 = </span>"+ Math.abs(-786);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs'>Learn More</a>";
+}
+
+function acos()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.acos </span>: <br> Returns the arc cosine (or inverse cosine) of a number.<br> <br> <span class='example'>Example: acos of adjacent=8 and hypotenuse=10 is = </span>"+ Math.acos(8/10);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/acos'>Learn More</a>";
+}
+function acosh()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.acosh </span>: <br> Returns the inverse hyperbolic cosine of a number.<br> <br> <span class='example'>Example: acosh of 2 = </span>"+ Math.acosh(2);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/acosh'>Learn More</a>";
+}
+function asin()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.sin </span>: <br> Returns the arcsine of a number.<br> <br> <span class='example'>Example: asin of adjacent=8 and hypotenuse=10 is = </span>"+ Math.asin(8/10);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/asin'>Learn More</a>";
+}
+function asinh()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.asinh </span>: <br> Returns the hyperbolic sine of a number.<br> <br> <span class='example'>Example: asinh of 2 = </span>"+ Math.sinh(2);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/asinh'>Learn More</a>";
+}
+function atan()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.atan </span>: <br> Returns the arctangent of a number.<br> <br> <span class='example'>Example: atan of adjacent=8 and hypotenuse=10 is = </span>"+ Math.atan(8/10);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan'>Learn More</a>";
+}
+function atan2()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.atan2 </span>: <br> Returns the angle (in radians) from the X axis to a point.<br> <br> <span class='example'>Example: atan2 of x=45 and y=90 is = </span>"+ Math.atan2(45,90);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2'>Learn More</a>";
+}
+function atanh()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.atanh </span>: <br> Returns the inverse hyperbolic tangent of a number.<br> <br> <span class='example'>Example: atanh of 0.5 = </span>"+ Math.atanh(0.5);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atanh'>Learn More</a>";
+}
+function cbrt()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.cbrt </span>: <br> Returns an implementation-dependent approximation to the cube root of number.<br> <br> <span class='example'>Example: cbrt of 64 is = </span>"+ Math.cbrt(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/cbrt'>Learn More</a>";
+}
+function ceil()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.ceil </span>: <br> Returns the smallest integer greater than or equal to its numeric argument.<br> <br> <span class='example'>Example: ceilt of -7.004 is = </span>"+ Math.ceil(-7.004);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil'>Learn More</a>";
+}
+function clz32()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.clz32 </span>: <br> Returns the number of leading zero bits in the 32-bit binary representation of a number.<br> <br> <span class='example'>Example: clz32 of 64 is = </span>"+ Math.clz32(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/clz32'>Learn More</a>";
+}
+function cos()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.cos </span>: <br> Returns the cosine of a number.<br> <br> <span class='example'>Example: cost of 64 is = </span>"+ Math.cos(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/cos'>Learn More</a>";
+}
+function cosh()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.cosh </span>: <br> Returns the hyperbolic cosine of a number.<br> <br> <span class='example'>Example: cosh of 64 is = </span>"+ Math.cosh(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/cosh'>Learn More</a>";
+}
+function exp()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.exp </span>: <br> Returns e (the base of natural logarithms) raised to a power.<br> <br> <span class='example'>Example: exp of 64 is = </span>"+ Math.exp(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/exp'>Learn More</a>";
+}
+function expm1()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.expm1 </span>: <br> Returns the result of (e^x - 1), which is an implementation-dependent approximation to subtracting 1 from the exponential function of x (e raised to the power of x, where e is the base of the natural logarithms).<br> <br> <span class='example'>Example: expm1 of 64 is = </span>"+ Math.expm1(69);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/expm1'>Learn More</a>";
+}
+function floor()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.floor </span>: <br> Returns the greatest integer less than or equal to its numeric argument.<br> <br> <span class='example'>Example: floor of 69.009 is = </span>"+ Math.floor(69.009);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor'>Learn More</a>";
+}
+function fround()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.fround </span>: <br> Returns the nearest single precision float representation of a number.<br> <br> <span class='example'>Example: fround of 64.890 is = </span>"+ Math.fround(64.890);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/fround'>Learn More</a>";
+}
+function hypot()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.hypot </span>: <br> Returns the square root of the sum of squares of its arguments.Values to compute the square root for. If no arguments are passed, the result is +0. If there is only one argument, the result is the absolute value. If any argument is +Infinity or -Infinity, the result is +Infinity. If any argument is NaN, the result is NaN. If all arguments are either +0 or −0, the result is +0.<br> <br> <span class='example'>Example: hypot of 5 and 12 is = </span>"+ Math.hypot(5,12);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/hypot'>Learn More</a>";
+}
+function imul()
+{
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.imul </span>: <br> Returns the result of 32-bit multiplication of two numbers.<br> <br> <span class='example'>Example: imul of 6 and 4 is = </span>"+ Math.imul(6,4);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul'>Learn More</a>";
+}
+
+function log(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.log </span>: <br>Returns the natural logarithm (base e) of a number.<br> <br> <span class='example'>Example: log of 6 is = </span>"+ Math.log(6);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/log'>Learn More</a>";
+}
+function log10(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.log10 </span>: <br>Returns the base 10 logarithm of a number.<br> <br> <span class='example'>Example: log10 of 6 is = </span>"+ Math.log10(6);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/log10'>Learn More</a>";
+}
+function log1p(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.log1p </span>: <br>Returns the natural logarithm of 1 + x.<br> <br> <span class='example'>Example: log1p of 6 is = </span>"+ Math.log1p(6);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/log1p'>Learn More</a>";
+}
+function log2(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.log2 </span>: <br>Returns the base 2 logarithm of a number.<br> <br> <span class='example'>Example: log2 of 6 is = </span>"+ Math.log2(6);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/log2'>Learn More</a>";
+}
+function max(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.max </span>: <br>Returns the larger of a set of supplied numeric expressions.<br> <br> <span class='example'>Example: max of 6,3,5,7,8,9 and 1 is = </span>"+ Math.max(6,3,5,7,8,9,1);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max'>Learn More</a>";
+}
+function min(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.min </span>: <br>Returns the smaller of a set of supplied numeric expressions.<br> <br> <span class='example'>Example: min of 6,3,5,7,8,9 and 1 is = </span>"+ Math.min(6,3,5,7,8,9,1);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min'>Learn More</a>";
+}
+function pow(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.pow </span>: <br>Returns the value of a base expression taken to a specified power.<br> <br> <span class='example'>Example: 7<sup>3</sup> is = </span>"+ Math.pow(7,3);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow'>Learn More</a>";
+}
+function random(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.random </span>: <br>Returns a pseudorandom number between 0 and 1.<br> <br> <span class='example'>Example:- </span>"+ Math.random();
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random'>Learn More</a>";
+}
+function round(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.round </span>: <br>Returns a supplied numeric expression rounded to the nearest integer.<br> <br> <span class='example'>Example: round of 8.076 is = </span>"+ Math.round(8.076);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round'>Learn More</a>";
+}
+function sign(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.sign </span>: <br>Returns the sign of the x, indicating whether x is positive, negative or zero.<br> <br> <span class='example'>Example: sign of -98 is = </span>"+ Math.sign(-98);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign'>Learn More</a>";
+}
+function sin(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.sin </span>: <br>Returns the sine of a number.<br> <br> <span class='example'>Example: sin of 64 is = </span>"+ Math.sin(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sin'>Learn More</a>";
+}
+function sinh(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.sinh </span>: <br>Returns the hyperbolic sine of a number.<br> <br> <span class='example'>Example: sinh of 64 is = </span>"+ Math.sinh(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sinh'>Learn More</a>";
+}
+function sqrt(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.sqrt </span>: <br>Returns the square root of a number.<br> <br> <span class='example'>Example: sqrt of 64 is = </span>"+ Math.sqrt(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt'>Learn More</a>";
+}
+function tan(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.tan </span>: <br>Returns the tangent of a number.<br> <br> <span class='example'>Example: tan of 64 is = </span>"+ Math.tan(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/tan'>Learn More</a>";
+}
+function tanh(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.tanh </span>: <br>Returns the hyperbolic tangent of a number.<br> <br> <span class='example'>Example: tanh of 64 is = </span>"+ Math.tanh(64);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/tanh'>Learn More</a>";
+}
+function trunc(){
+    explain.innerHTML = "<span class = 'property'>(method)</span> <br> <br> <span class = 'tile'> Math.trunc </span>: <br>Returns the integral part of the a numeric expression, x, removing any fractional digits. If x is already an integer, the result is x.<br> <br> <span class='example'>Example: trunc of 42.84 is = </span>"+ Math.trunc(42.84);
+    lrn.innerHTML ="<a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc'>Learn More</a>";
+}
+
+
